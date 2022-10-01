75ebd82b1d5dec61ab018523cdeef53d82689115
Erick <efmz2000@outlook.es>
2022-10-01 13:16:00 -0600

    WEBSITE changes

--- src/server/website/js/PerXProp.js
+++ src/server/website/js/PerXProp.js
@@ -3,18 +3,16 @@ $(document).ready(function(){
     if(add== "0"){
       var idPer = (new URL(location.href)).searchParams.get('nombre')
       var idProp = (new URL(location.href)).searchParams.get('lote')
-      var fechaI = (new URL(location.href)).searchParams.get('fechaI')
-      var fechaF = (new URL(location.href)).searchParams.get('fechaF')
-      document.getElementById('nombre').value =idPer;
-      document.getElementById('lote').value =idProp;
-      document.getElementById('fechaI').value =fechaI;
-      document.getElementById('fechaF').value =fechaF;
+      nombre = document.getElementById('nombre')
+      nombre.value = idPer
+      lote = document.getElementById('lote')
+      lote.value =idProp
     }
 })
 
 function ret() {
   var user = ''// (new URL(location.href)).searchParams.get('user')
-  location.replace(' ./listaPerXProp.html?user='+user);
+  location.replace(' ./listaPerXProp.html?user='+user)
 }
 function cerrar(){
   location.replace('./index.html');
--- src/server/website/js/listaUsersXprop.js
+++ src/server/website/js/listaUsersXprop.js
@@ -1,17 +1,17 @@
-$( document ).ready(function() {
-    $('.linea').click(function() {
-        $('#home').html('<embed src="./files/'+this.id+'_4b/'+this.id+'_ejemplo.pdf" type="application/pdf" width="100%" height="400px" class="margenfilemodal" />');
-        //alert(this);
-        let elementos = this.getElementsByTagName("td")
-
-
-        let url = './UsersXprop.html?add=0'
-
-        let usuario = '&usuario='+ elementos[0].innerHTML
-        let lote = '&lote='+ elementos[1].innerHTML
-        location.replace(url+usuario+lote);
-    });
-});
+//$( document ).ready(function() {
+//    $('.linea').click(function() {
+//        $('#home').html('<embed src="./files/'+this.id+'_4b/'+this.id+'_ejemplo.pdf" type="application/pdf" width="100%" height="400px" class="margenfilemodal" />');
+//        //alert(this);
+//        let elementos = this.getElementsByTagName("td")
+//
+//
+//        let url = './UsersXprop.html?add=0'
+//
+//        let usuario = '&usuario='+ elementos[0].innerHTML
+//        let lote = '&lote='+ elementos[1].innerHTML
+//        location.replace(url+usuario+lote);
+//    });
+//});
 function edit(usuario,lote){
   let url = './UserXpropEdit.html?add=0'
   url+='&usuario='+usuario
