8352a57397ab763af9dd0476e8d1094ddd2e0fe0
Erick <efmz2000@outlook.es>
2022-10-30 23:12:51 -0600

    event log

--- src/server/website/js/EventLog.js
+++ src/server/website/js/EventLog.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
     .then(response => {
       $("#tableBody > tbody").empty();
       for (var i = 0; i < response.length; i++) {
-        console.log(response[i])
         var resp = response [i];      
         var tipo = resp.TipodeEvento;
         var Entidad = resp.Entidad;
@@ -19,7 +18,8 @@ $(document).ready(function(){
         var Actualizacion = resp.Actualizacion 
         var Autor = resp.Autor;
         var IP = resp.IP;
-        var Fecha = resp.Fecha.substring(0,10);
+        var Fecha = resp.Fecha.substring(0,10);        
+                 
         var tabla = "<tr><td> ";
         tabla += tipo + "</td><td>" + Entidad + "</td><td>" + IDEntidad + "</td><td width=\"30%\">" + Historial + "</td><td width=\"30%\">" + Actualizacion + "</td><td>" + Autor + "</td><td>" + IP + "</td><td>" + Fecha+ "</td>"
          //se debe cambiar el otro boton       
