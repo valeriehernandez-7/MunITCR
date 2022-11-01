89f809305e2aec7d1bb53378dacb2cb8fa57acde
valeriehernandez-7 <valerie.herfer@gmail.com>
2022-10-31 22:56:49 -0600

    SP_Pago Added

---
+++ src/database/sql-queries/stored-procedures/ComprobantePago/SP_Pago.sql
@@ -0,0 +1,165 @@
+/*DB Access*/
+USE [MunITCR]
+GO
+
+/* 
+	@proc_name SP_Pago
+	@proc_description
+	@proc_param inPropiedadLote 
+	@proc_param inReferencia 
+	@proc_param inMedioPago 
+	@proc_param inFecha 
+	@proc_param outResultCode Procedure return value
+	@author <a href="https://github.com/valeriehernandez-7">Valerie M. Hernández Fernández</a>
+*/
+CREATE OR ALTER PROCEDURE [SP_Pago]
+	@inPropiedadLote VARCHAR(32),
+	@inReferencia BIGINT,
+	@inMedioPago VARCHAR(128),
+	@inFecha DATE,
+	@outResultCode INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SET @outResultCode = 0; /* Unassigned code */
+
+		IF (@inPropiedadLote IS NOT NULL) AND (@inMedioPago IS NOT NULL)
+			BEGIN
+				/* Gets the PK of "Propiedad" using @inPropiedadLote */
+				DECLARE @idPropiedad INT;
+				SELECT @idPropiedad = [Pro].[ID]
+				FROM [dbo].[Propiedad] AS [Pro]
+				WHERE [Pro].[Lote] = @inPropiedadLote;
+
+				/* Gets the PK of "Medio de Pago" using @inMedioPago */
+				DECLARE @idMedioPago INT;
+				SELECT @idMedioPago = [MP].[ID]
+				FROM [dbo].[MedioPago] AS [MP]
+				WHERE [MP].[Nombre] = @inMedioPago;
+
+				IF @inReferencia IS NULL
+					BEGIN
+						DECLARE @referenciaStr VARCHAR(16) = CONCAT(CONVERT(VARCHAR, GETDATE(), 112), '000000');
+						DECLARE @referenciaInt BIGINT = CAST(@referenciaStr AS BIGINT);
+						/* Create new "referencia de comprobante de pago" */
+						WHILE EXISTS (SELECT [CP].[Referencia] 
+						FROM [dbo].[ComprobantePago] AS [CP] 
+						WHERE [CP].[Referencia] = @referenciaInt)
+							BEGIN
+								SET @referenciaInt = @referenciaInt + 1;
+							END;
+						/* Set the generated reference as the "Referencia" */
+						SET @inReferencia = @referenciaInt;
+					END;
+				
+				IF @inFecha IS NULL
+					BEGIN
+						SET @inFecha = GETDATE();
+					END;
+				
+				IF (@idPropiedad IS NOT NULL) AND (@idMedioPago IS NOT NULL) 
+				AND (@inReferencia IS NOT NULL) AND (@inFecha IS NOT NULL)
+					BEGIN
+						/* Get the oldest "fecha de factura" of 
+						"facturas pendientes" "no anuladas" of the property 
+						with @idPropiedad as PK */
+						DECLARE @fechaFactura DATE;
+						SELECT @fechaFactura = MIN([F].[Fecha])
+						FROM [dbo].[Factura] AS [F]
+							INNER JOIN [dbo].[Propiedad] AS [P]
+							ON [P].[ID] = [F].[IDPropiedad]
+						WHERE [P].[ID] = @idPropiedad 
+						AND [F].[IDComprobantePago] IS NULL 
+						AND [F].[Activo] = 1;
+
+						IF @fechaFactura IS NOT NULL
+							BEGIN
+								BEGIN TRANSACTION [createPago]
+									INSERT INTO [dbo].[ComprobantePago] (
+										[IDMedioPago],
+										[Referencia],
+										[Fecha]
+									) VALUES (
+										@idMedioPago,
+										@inReferencia,
+										@inFecha
+									);
+									
+									UPDATE [dbo].[Factura]
+										SET [IDComprobantePago] = SCOPE_IDENTITY()
+									FROM [dbo].[Factura] AS [F]
+										INNER JOIN [dbo].[Propiedad] AS [P]
+										ON [P].[ID] = [F].[IDPropiedad]
+									WHERE [P].[ID] = @idPropiedad 
+									AND [F].[IDComprobantePago] IS NULL 
+									AND [F].[Fecha] = @fechaFactura 
+									AND [F].[Activo] = 1;
+									
+									SET @outResultCode = 5200; /* OK */
+								COMMIT TRANSACTION [createPago]
+							END;
+						ELSE
+							BEGIN
+								/* Cannot insert the new "ComprobantePago" because a
+								there is no @fechaFactura to insert use as 
+								reference for the Factura */
+								SET @outResultCode = 5405; 
+								RETURN;
+							END;
+					END;
+				ELSE
+					BEGIN
+						/* Cannot insert the new "ComprobantePago" because a 
+						"Medio de pago" with name @inMedioPago did not exist or 
+						@inFecha is null or @inReferencia is null */
+						SET @outResultCode = 5404; 
+						RETURN;
+					END;
+			END;
+		ELSE
+			BEGIN
+				/* Cannot insert the new "Comprobante de Pago" 
+				because some params are null */
+				SET @outResultCode = 5400; 
+				RETURN;
+			END;
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+			BEGIN
+				ROLLBACK TRANSACTION [createPago]
+			END;
+		IF OBJECT_ID(N'dbo.ErrorLog', N'U') IS NOT NULL /* Check Error table existence */
+			BEGIN
+				/* Update Error table */
+				INSERT INTO [dbo].[ErrorLog] (
+					[Username],
+					[ErrorNumber],
+					[ErrorState],
+					[ErrorSeverity],
+					[ErrorLine],
+					[ErrorProcedure],
+					[ErrorMessage],
+					[ErrorDateTime]
+				) VALUES (
+					SUSER_NAME(),
+					ERROR_NUMBER(),
+					ERROR_STATE(),
+					ERROR_SEVERITY(),
+					ERROR_LINE(),
+					ERROR_PROCEDURE(),
+					ERROR_MESSAGE(),
+					GETDATE()
+				);
+				SET @outResultCode = 5504; /* CHECK ErrorLog */
+			END;
+		ELSE 
+			BEGIN
+				SET @outResultCode = 5404; /* ERROR : dbo.ErrorLog DID NOT EXIST */
+				RETURN;
+			END;
+	END CATCH;
+	SET NOCOUNT OFF;
+END;
+GO
