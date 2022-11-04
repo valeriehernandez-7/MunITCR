4f49993e063aad52a7763db95e694a458bb1ff90
valeriehernandez-7 <valerie.herfer@gmail.com>
2022-11-04 16:19:55 -0600

    Run Script - INSERT @ TMP

--- src/database/sql-queries/scripts/Run.sql
+++ src/database/sql-queries/scripts/Run.sql
@@ -65,15 +65,6 @@ DECLARE @TMPActualizacionPropiedad TABLE (
 	[ValorFiscal] MONEY
 );
 
-/* Tabla con asociaciones o desasociaciones de personas 
-con propiedades del archivo Operaciones.xml */
-DECLARE @TMPPersonaXPropiedad TABLE (
-	[ID] INT IDENTITY(1,1) PRIMARY KEY,
-	[TipoOperacion] VARCHAR(8),
-	[PersonaValorDocIdentidad] VARCHAR(64),
-	[PropiedadLote] VARCHAR(32)
-);
-
 /* Tabla con informacion de usuarios y su relacion con 
 persona del archivo Operaciones.xml */
 DECLARE @TMPUsuario TABLE (
@@ -83,6 +74,15 @@ DECLARE @TMPUsuario TABLE (
 	[Username] VARCHAR(16),
 	[Password] VARCHAR(16),
 	[TipoUsuario] VARCHAR(16)
+);
+
+/* Tabla con asociaciones o desasociaciones de personas 
+con propiedades del archivo Operaciones.xml */
+DECLARE @TMPPersonaXPropiedad TABLE (
+	[ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[TipoOperacion] VARCHAR(8),
+	[PersonaValorDocIdentidad] VARCHAR(64),
+	[PropiedadLote] VARCHAR(32)
 );
 
 /* Tabla con asociaciones o desasociaciones de usuarios 
@@ -107,6 +107,7 @@ DECLARE @TMPMovimientoConsumoAgua TABLE (
 por propiedad del archivo Operaciones.xml */
 DECLARE @TMPComprobantePago TABLE (
 	[ID] INT IDENTITY(1,1) PRIMARY KEY,
+	[PropiedadLote] VARCHAR(32),
 	[MedioPago] VARCHAR(128),
 	[Referencia] BIGINT
 );
@@ -124,15 +125,17 @@ FROM @data.nodes('Datos/Operacion') AS DOC(Fecha);
 SELECT * FROM @TMPOperacion; -- TO-DO: DELETE
 
 DECLARE 
-	@minIDOperacion INT,
-	@maxIDOperacion INT,
-	@minMesOperacion INT,
-	@maxMesOperacion INT,
-	@fechaOperacion DATE,
-	@fechaFinMes DATE;
+	@minTMPID INT, -- PK del registro en iteracion
+	@maxTMPID INT, -- PK del ultimo registro por iterar
+	@minIDOperacion INT, -- PK de la operacion en iteracion de @TMPOperacion
+	@maxIDOperacion INT, -- PK de la ultima operacion por iterar en @TMPOperacion
+	@minMesOperacion INT, -- mes de operaciones en iteracion de @TMPOperacion
+	@maxMesOperacion INT, -- ultimo mes de operaciones por iterar en @TMPOperacion
+	@fechaOperacion DATE, -- fecha actual en el procesamiento de operaciones
+	@fechaFinMes DATE; -- fecha de fin de mes en iteracion
 
 
-/* Obtiene el ID minimo y maximo de la tabla de operaciones 
+/* Obtiene el Mes minimo y maximo de la tabla de operaciones 
 como limites de iteracion para la ejecucion de la 
 simulacion de operaciones */
 SELECT 
@@ -168,28 +171,151 @@ WHILE (@minMesOperacion <= @maxMesOperacion)
 				FROM @TMPOperacion AS [O]
 				WHERE [O].[ID] = @minIDOperacion;
 
+
 				/* <Personas/Persona> : Procesar personas */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Personas/Persona') = 1
+					BEGIN
+						INSERT INTO @TMPPersona (
+							[TipoDocIdentidad],
+							[Nombre],
+							[ValorDocIdentidad],
+							[Telefono1],
+							[Telefono2],
+							[CorreoElectronico]
+						) SELECT
+							OP.Persona.value('@TipoDocumentoIdentidad', 'VARCHAR(128)'),
+							OP.Persona.value('@Nombre', 'VARCHAR(128)'),
+							OP.Persona.value('@ValorDocumentoIdentidad', 'VARCHAR(64)'),
+							OP.Persona.value('@Telefono1', 'VARCHAR(16)'),
+							OP.Persona.value('@Telefono2', 'VARCHAR(16)'),
+							OP.Persona.value('@Email', 'VARCHAR(256)')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Personas/Persona') AS OP(Persona);
+					END;
+
 
 				/* <Propiedades/Propiedad> : Procesar propiedades */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Propiedades/Propiedad') = 1
+					BEGIN
+						INSERT INTO @TMPPropiedad (
+							[TipoUsoPropiedad],
+							[TipoZonaPropiedad],
+							[Lote],
+							[Medidor],
+							[MetrosCuadrados],
+							[ValorFiscal]
+						) SELECT
+							OP.Propiedad.value('@tipoUsoPropiedad', 'VARCHAR(128)'),
+							OP.Propiedad.value('@tipoZonaPropiedad', 'VARCHAR(128)'),
+							OP.Propiedad.value('@NumeroFinca', 'VARCHAR(32)'),
+							OP.Propiedad.value('@NumeroMedidor', 'VARCHAR(16)'),
+							OP.Propiedad.value('@MetrosCuadrados', 'BIGINT'),
+							OP.Propiedad.value('@ValorFiscal', 'MONEY')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Propiedades/Propiedad') AS OP(Propiedad);
+					END;
+
 
 				/* <PropiedadCambio/PropiedadCambios> : 
 				Procesar cambios de valor fiscal de propiedad */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PropiedadCambio/PropiedadCambios') = 1
+					BEGIN
+						INSERT INTO @TMPActualizacionPropiedad (
+							[Lote],
+							[ValorFiscal]
+						) SELECT
+							OP.ActualizacionPropiedad.value('@NumFinca', 'VARCHAR(32)'),
+							OP.ActualizacionPropiedad.value('@Valor', 'MONEY')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PropiedadCambio/PropiedadCambios') AS OP(ActualizacionPropiedad);
+					END;
 
-				/* <PersonasyPropiedades/PropiedadPersona> : 
-				Procesar asociación o des asociación entre personas y propiedades */
 
 				/* <Usuario/Usuario> : 
 				Procesar asociación o des asociación entre usuarios y personas */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Usuario/Usuario') = 1
+					BEGIN
+						INSERT INTO @TMPUsuario (
+							[TipoOperacion],
+							[PersonaValorDocIdentidad],
+							[Username],
+							[Password],
+							[TipoUsuario]
+						) SELECT
+							OP.Usuario.value('@TipoAsociacion', 'VARCHAR(8)'),
+							OP.Usuario.value('@ValorDocumentoIdentidad', 'VARCHAR(64)'),
+							OP.Usuario.value('@Username', 'VARCHAR(16)'),
+							OP.Usuario.value('@Password', 'VARCHAR(16)'),
+							OP.Usuario.value('@TipoUsuario', 'VARCHAR(16)')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Usuario/Usuario') AS OP(Usuario);
+					END;
+
+
+				/* <PersonasyPropiedades/PropiedadPersona> : 
+				Procesar asociación o des asociación entre personas y propiedades */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PersonasyPropiedades/PropiedadPersona') = 1
+					BEGIN
+						INSERT INTO @TMPPersonaXPropiedad (
+							[TipoOperacion],
+							[PersonaValorDocIdentidad],
+							[PropiedadLote]
+						) SELECT
+							OP.PersonaPropiedad.value('@TipoAsociacion', 'VARCHAR(8)'),
+							OP.PersonaPropiedad.value('@ValorDocumentoIdentidad', 'VARCHAR(64)'),
+							OP.PersonaPropiedad.value('@NumeroFinca', 'VARCHAR(32)')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PersonasyPropiedades/PropiedadPersona') AS OP(PersonaPropiedad);
+					END;
+
 
 				/* <PropiedadesyUsuarios/UsuarioPropiedad> : 
 				Procesar asociación o des asociación entre usuarios y propiedades */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PropiedadesyUsuarios/UsuarioPropiedad') = 1
+					BEGIN
+						INSERT INTO @TMPUsuarioXPropiedad (
+							[TipoOperacion],
+							[PersonaValorDocIdentidad],
+							[PropiedadLote]
+						) SELECT
+							OP.UsuarioPropiedad.value('@TipoAsociacion', 'VARCHAR(8)'),
+							OP.UsuarioPropiedad.value('@ValorDocumentoIdentidad', 'VARCHAR(64)'),
+							OP.UsuarioPropiedad.value('@NumeroFinca', 'VARCHAR(32)')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/PropiedadesyUsuarios/UsuarioPropiedad') AS OP(UsuarioPropiedad);
+					END;
+
 
 				/* <Lecturas/LecturaMedidor> : 
 				Procesar lecturas de medidor de propiedad */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Lecturas/LecturaMedidor') = 1
+					BEGIN
+						INSERT INTO @TMPMovimientoConsumoAgua (
+							[TipoMovimientoConsumoAgua],
+							[Medidor],
+							[LecturaMedidor]
+						) SELECT
+							OP.ConsumoAgua.value('@TipoMovimiento', 'VARCHAR(128)'),
+							OP.ConsumoAgua.value('@NumeroMedidor', 'VARCHAR(16)'),
+							OP.ConsumoAgua.value('@Valor', 'INT')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Lecturas/LecturaMedidor') AS OP(ConsumoAgua);
+					END;
 
-				/* <Pago/Pago> : Procesar pagos factura mas vieja por propiedad con medio de pago <> 'Arreglo de pago' */
 
-				/* <Pago/Pago> : Procesar pagos factura mas vieja por propiedad con medio de pago 'Arreglo de pago' */
+				/* <Pago/Pago> : Procesar pagos factura mas vieja por propiedad */
+				IF @data.exist('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Pago/Pago') = 1
+					BEGIN
+						INSERT INTO @TMPComprobantePago (
+							[PropiedadLote],
+							[MedioPago],
+							[Referencia] 
+						) SELECT
+							OP.Pago.value('@NumFinca', 'VARCHAR(32)'),
+							OP.Pago.value('@TipoPago', 'VARCHAR(128)'),
+							OP.Pago.value('@NumeroReferenciaComprobantePago', 'BIGINT')
+						FROM @data.nodes('/Datos/Operacion[@Fecha = sql:variable("@fechaOperacion")]/Pago/Pago') AS OP(Pago);
+
+						/* <Pago/Pago> : Procesar pagos factura mas vieja por propiedad con medio de pago <> 'Arreglo de pago' */
+
+
+						/* <Pago/Pago> : Procesar pagos factura mas vieja por propiedad con medio de pago 'Arreglo de pago' */
+
+					END;
+
 
 				/* Procesar ordenes de reconexion de consumo de agua */
 
@@ -207,8 +333,8 @@ WHILE (@minMesOperacion <= @maxMesOperacion)
 				DELETE FROM @TMPPersona;
 				DELETE FROM @TMPPropiedad;
 				DELETE FROM @TMPActualizacionPropiedad;
-				DELETE FROM @TMPPersonaXPropiedad;
 				DELETE FROM @TMPUsuario;
+				DELETE FROM @TMPPersonaXPropiedad;
 				DELETE FROM @TMPUsuarioXPropiedad;
 				DELETE FROM @TMPMovimientoConsumoAgua;
 				DELETE FROM @TMPComprobantePago;
