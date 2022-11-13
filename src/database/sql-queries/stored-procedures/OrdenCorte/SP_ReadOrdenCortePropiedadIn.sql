b9ed72a8c5ced8267d5248e26ea511a1745f28ea
valeriehernandez-7 <valerie.herfer@gmail.com>
2022-11-13 01:17:24 -0600

    SP_ReadOrdenCorte & SP_ReadOrdenReconexion Added

---
+++ src/database/sql-queries/stored-procedures/OrdenCorte/SP_ReadOrdenCortePropiedadIn.sql
@@ -0,0 +1,74 @@
+/*DB Access*/
+USE [MunITCR]
+GO
+
+/* 
+	@proc_name SP_ReadOrdenCortePropiedadIn
+	@proc_description
+	@proc_param inPropiedadLote
+	@proc_param outResultCode Procedure return value
+	@author <a href="https://github.com/valeriehernandez-7">Valerie M. Hernández Fernández</a>
+*/
+CREATE OR ALTER PROCEDURE [SP_ReadOrdenCortePropiedadIn]
+	@inPropiedadLote VARCHAR(32),
+	@outResultCode INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY
+		SET @outResultCode = 0; /* Unassigned code */
+		IF (@inPropiedadLote IS NOT NULL)
+			BEGIN
+				SELECT 
+					[OC].[Fecha] AS [SuspensiondeServicio],
+					[F].[FechaVencimiento] AS [VencimientodeFactura],
+					(CASE 
+						WHEN [OC].[Activo] = 0 THEN 'Realizado'
+						WHEN [OC].[Activo] = 1 THEN 'Pendiente' 
+					END) AS [EstadodeReconexion]
+				FROM [dbo].[OrdenCorte] AS [OC]
+					INNER JOIN [dbo].[Factura] AS [F]
+					ON [F].[ID] = [OC].[IDFactura]
+					INNER JOIN [dbo].[Propiedad] AS [P]
+					ON [P].[ID] = [F].[IDPropiedad]
+				WHERE [P].[Lote] = @inPropiedadLote
+				AND [P].[Activo] = 1
+				AND [F].[Activo] = 1
+				ORDER BY [OC].[Fecha] DESC;
+				SET @outResultCode = 5200; /* OK */
+			END;
+	END TRY
+	BEGIN CATCH
+		IF OBJECT_ID(N'dbo.ErrorLog', N'U') IS NOT NULL /* Check Error table existence */
+			BEGIN
+				/* Update Error table */
+				INSERT INTO [dbo].[ErrorLog] (
+					[Username],
+					[ErrorNumber],
+					[ErrorState],
+					[ErrorSeverity],
+					[ErrorLine],
+					[ErrorProcedure],
+					[ErrorMessage],
+					[ErrorDateTime]
+				) VALUES (
+					SUSER_NAME(),
+					ERROR_NUMBER(),
+					ERROR_STATE(),
+					ERROR_SEVERITY(),
+					ERROR_LINE(),
+					ERROR_PROCEDURE(),
+					ERROR_MESSAGE(),
+					GETDATE()
+				);
+				SET @outResultCode = 5504; /* CHECK ErrorLog */
+			END;
+		ELSE 
+			BEGIN
+				SET @outResultCode = 5404; /* ERROR : dbo.ErrorLog DID NOT EXIST */
+				RETURN;
+			END;
+	END CATCH;
+	SET NOCOUNT OFF;
+END;
+GO
